3a43539e2cb215cfa42d54a3fccbbf352a535791
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-05-27 06:14:48 +0300

    add: search input

--- src/app/components/main/search-input/search-input.component.ts
+++ src/app/components/main/search-input/search-input.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { WordResponse } from 'src/app/models/word-response';
+import { WordRequestService } from 'src/app/services/word-request.service';
 
 @Component({
   selector: 'sozluk-search-input',
@@ -6,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-input.component.scss']
 })
 export class SearchInputComponent {
+  constructor(private wordRequest: WordRequestService, private route: Router) { }
+  @ViewChild("searchForm") searchForm!: NgForm;
+  wordInput!: string;
+  wordResponse !: Array<WordResponse>;
+  async onSubmit() {
 
+    this.route.navigate([`${this.wordInput}`])
+  }
+  @HostListener('window:keydown.enter', ['$event'])
+  onEnter(event: KeyboardEvent) {
+    if (event.target instanceof HTMLInputElement) {
+      // when word searched by pressing enter on input element, submit the form and change the focus so that input validation error doesn't appear when not focused
+      this.onSubmit()
+      event.target.blur()
+      this.searchForm.reset()
+    }
+  }
+  public get isInputValid(): boolean {
+    let wordFiltered = this.wordInput?.trim()
+    return wordFiltered?.length > 0 ? true : false;
+  }
 }
